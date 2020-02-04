2ebad3072df1bacc4852a729187d615ed06456d5
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2020-02-04 18:25:58 +0900

    Update issue.py

--- issue.py
+++ issue.py
@@ -38,8 +38,7 @@ async def on_message(message):
     if message.content == "is!help":
         embed = discord.Embed(title="Issue bot ヘルプ",description="'い' or 'し' or 'ゅ' or 'ー' or 'いしゅー'で反応するよ",color=#00BB00)
         embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/663113701859131410/674175699367755796/BOT.png")
-        embed.add_field(name="meet",value="you")
-        #embed.add_field(name="issue-global",value="上記の名前でチャンネルを作るとグローバルチャットが使えるよ")
+        embed.add_field(name="issue-global",value="上記の名前でチャンネルを作るとグローバルチャットが使えるよ", inline=False)
         await channel.send(embed=embed)
 
     if message.content == 'い' or 'し' or 'ゅ' or 'ー' or 'いしゅー': 
