25c74b9546b6b58c32b56620be078695b80b2274
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2020-02-04 20:56:46 +0900

    Update issue.py

--- issue.py
+++ issue.py
@@ -39,7 +39,7 @@ async def on_message(message):
         embed = discord.Embed(title="Issue bot ヘルプ",description="｢い｣｢し｣｢ゅ｣｢ー｣｢いしゅー｣で反応するよ。\n発言すると覚悟の有無を聞かれるけれど、｢y｣と発言すれば開始するよ。",color=0x2ecc71)
         embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/670982490999226370/674193654344056842/Screenmemo_2020-02-04-18-00-12.png")
         embed.add_field(name="**issue-global**",value="上記の名前でチャンネルを作ると自動でグローバルチャットに接続されます。",inline=False)
-        embed.set_author(name="BOT招待URL",url="https://discordapp.com/api/oauth2/authorize?client_id=674176006801850369&permissions=1812987088&scope=bot",icon_url="https://bit.ly/2SsIBiC")
+        embed.set_footer(text="BOT招待URL",icon_url="https://discordapp.com/api/oauth2/authorize?client_id=674176006801850369&permissions=1812987088&scope=bot")
         await message.channel.send(embed=embed)
 
     if message.content == 'い' or message.content == 'し' or message.content == 'ゅ' or message.content == 'ー' or message.content == 'いしゅー': 
