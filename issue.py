e5a7eaa92997bbd762ce97a8a7572d3736222f87
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2020-02-05 16:43:45 +0900

    Update issue.py

--- issue.py
+++ issue.py
@@ -5,7 +5,6 @@ from datetime import datetime
 
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
 
-GLOBAL_CH_NAME = "issue-global"
 ISS_SRART = "issue-start"
 
 # 接続に必要なオブジェクトを生成
@@ -68,37 +67,5 @@ async def on_message(message):
                 while issue_counter < 51:
                     await message.channel.send( "いしゅー" )
                     issue_counter = issue_counter + 1
-
-    #-----------グローバルチャット-----------
-    if message.channel.name == GLOBAL_CH_NAME:
-        # issue-globalの名前をもつチャンネルに投稿されたので、メッセージを転送する
-
-        await message.delete() # 元のメッセージは削除しておく
-
-        if 'discord.gg' in message.content:
-            await message.channel.send("ここで招待は送れません。")
-            return # 招待は送れません
-
-        if message.content == "is!help":
-            return
-
-        channels = client.get_all_channels()
-        # channelsはbotの取得できるチャンネルのイテレーター
-        global_channels = [ch for ch in channels if ch.name == GLOBAL_CH_NAME]
-        # global_channelsは issue-global の名前を持つチャンネルのリスト
-
-        embed = discord.Embed(title="いしゅー",
-            description=message.content, color=0x2ecc71)
-
-        embed.set_author(name=message.author.display_name, 
-            icon_url=message.author.avatar_url_as(format="png"))
-        embed.set_footer(text=f"{message.guild.name} / {message.channel.name}",
-            icon_url=message.guild.icon_url_as(format="png"))
-        # Embedインスタンスを生成、投稿者、投稿場所などの設定
-
-        for channel in global_channels:
-            # メッセージを埋め込み形式で転送
-            await channel.send(embed=embed)
-#-----------グローバルチャット-----------
               
 client.run(TOKEN)
