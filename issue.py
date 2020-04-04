09a44d6fc14b51e6b11d39ad80ebe9849472356b
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-04-04 17:27:43 +0900

    Update issue.py

--- issue.py
+++ issue.py
@@ -1,73 +1,47 @@
-import discord
+from discord.ext import commands, tasks # Bot Commands Frameworkをインポート
+import traceback # エラー表示のためにインポート
 import os
-import asyncio
-from datetime import datetime
+import discord
 
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
 
-GLOBAL_CH_NAME = "issue-global"
-ISS_SRART = "issue-start"
+# 読み込むコグの名前を格納しておく。
+INITIAL_EXTENSIONS = [
+    'cog.issue'
+]
 
-# 接続に必要なオブジェクトを生成
-client = discord.Client()
+# クラスの定義。ClientのサブクラスであるBotクラスを継承。
+class MyBot(commands.Bot):
 
-@client.event
-async def on_ready():
-    print(client.user.name)  # ボットの名前
-    print(client.user.id)  # ボットのID
-    print(discord.__version__)  # discord.pyのバージョン
-    print('----------------')
-    print('Hello World,issue_bot started.')
-    channel = client.get_channel(674180325890457610)
-    date = datetime.now()
-    await channel.send(f'>>> **再起動情報** \n 名前:{client.user.name} \n ID:{client.user.id} \n Discord ver:{discord.__version__} \n ------------------------------- \n 状態：いしゅー \n \n 再起動時間：{date.year}年{date.month}月{date.day}日{date.hour}時{date.minute}分{date.second}秒')  # ボットの名前    
-    await client.change_presence(status=discord.Status.idle,activity=discord.Game(name='ヘルプ| is!help'))
+    # MyBotのコンストラクタ。
+    def __init__(self, command_prefix):
+        # スーパークラスのコンストラクタに値を渡して実行。
+        super().__init__(command_prefix)
 
-@client.event
-async def on_message(message):
-    
-    if message.author.bot:
-        # もし、送信者がbotなら無視する
-        return
+        # INITIAL_COGSに格納されている名前から、コグを読み込む。
+        # エラーが発生した場合は、エラー内容を表示。
+        for cog in INITIAL_EXTENSIONS:
+            try:
+                self.load_extension(cog)
+            except Exception:
+                traceback.print_exc()
 
-    if message.content == "is!help":
-        embed = discord.Embed(title="Issue bot ヘルプ",description="｢い｣｢し｣｢ゅ｣｢ー｣｢いしゅー｣で反応するよ。\n発言すると覚悟の有無を聞かれるけれど、｢y｣と発言すれば開始するよ。",color=0x2ecc71)
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/670982490999226370/674193654344056842/Screenmemo_2020-02-04-18-00-12.png")
-        embed.add_field(name="**issue-global**",value="上記の名前でチャンネルを作ると自動でグローバルチャットに接続されます。",inline=False)
-        embed.add_field(name="**各種リンク**", value="[BOT招待URL](<https://discordapp.com/api/oauth2/authorize?client_id=674176006801850369&permissions=1812987088&scope=bot>)", inline=False)  
-        await message.channel.send(embed=embed)
+    # Botの準備完了時に呼び出されるイベント
+    async def on_ready(self):
+        print(self.user.name)  # ボットの名前
+        print(self.user.id)  # ボットのID
+        print(discord.__version__)  # discord.pyのバージョン
+        print('----------------')
+        print('いしゅー')
+        channel = self.get_channel(694452244635975691)
+        await channel.send(self.user.name)  # ボットの名前
+        await channel.send(self.user.id)  # ボットのID
+        await channel.send(discord.__version__)  # discord.pyのバージョン
+        await channel.send('----------------')
+        await channel.send('いしゅー')
+        await self.change_presence(activity=discord.Game(name=f'ヘルプ|is!help')) 
 
-    if message.content == 'テスト':
-        if message.channel.name == GLOBAL_CH_NAME or message.channel.name == ISS_SRART:
-            await message.channel.send('ここでは使うことが出来ません。')
-            return
-        issue_test = 0
-        while issue_test < 25:
-            embed = discord.Embed(title=" ",description=" ")
-            embed.set_image(url="https://cdn.discordapp.com/attachments/674223403313659934/674581603540008970/9QfQXW91k2wAAAAASUVORK5CYII.png")
-            await message.channel.send(embed=embed)
-            issue_test = issue_test + 1
-
-    if message.content == 'い' or message.content == 'し' or message.content == 'ゅ' or message.content == 'ー' or message.content == 'いしゅー': 
-        if message.channel.name == GLOBAL_CH_NAME or message.channel.name == ISS_SRART:
-            await message.channel.send('ここでは使うことが出来ません。')
-            return
-        await message.channel.send('この後｢いしゅー｣が50回スパムされます。\n覚悟があるなら、｢y｣と発言してください。\n何も発言しない(10秒待機)すると停止します。') 
-        def  issuespam(m):
-            return m.content == "y" and m.author == message.author
-        try:
-            reply = await client.wait_for( "message" , check = issuespam , timeout = 10.0 )
-        except asyncio.TimeoutError:
-            await message.channel.send( "中止します。" )
-            return
-        else:
-            if not reply.content == "y":
-                await message.channel.send( "中止します。" )
-                return
-            elif reply.content == "y":
-                issue_counter =0
-                while issue_counter < 51:
-                    await message.channel.send( "いしゅー" )
-                    issue_counter = issue_counter + 1
-              
-client.run(TOKEN)
+#MyBotのインスタンス化及び起動処理。
+if __name__ == '__main__':
+    bot = MyBot(command_prefix='is!') # command_prefixはコマンドの最初の文字として使うもの。 e.g. !ping
+    bot.run(TOKEN) # Botのトークン
